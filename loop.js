4b8a7f20ed7e1d6d3d5dbc57f1daf219afc6f46f
Linh <linh.nguyenkhanhib@gmail.com>
2022-09-20 13:36:11 +0300

    loop assignment

---
+++ loop.js
@@ -0,0 +1,7 @@
+// Create a loop which prints only the odd numbers between 1 and 10, starting from 10 and counting down.
+var oddNum = [];
+for (let i = 10; i > 0; i--) {
+  if (i % 2 != 0) {
+    console.log(i);
+  }
+}
